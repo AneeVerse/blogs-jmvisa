fbf4d9ccdb499cf82dc22b5b190c0315e319a476
Ahmad Anees <83137173+AneeVerse@users.noreply.github.com>
2025-01-01 16:45:58 +0530

    a
    a

--- config/database.js
+++ config/database.js
@@ -10,7 +10,7 @@ module.exports = ({ env }) => {
         port: env.int('DATABASE_PORT', 25060),
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'doadmin'),
-        password: env('DATABASE_PASSWORD', ''),
+        password: env('DATABASE_PASSWORD',),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
